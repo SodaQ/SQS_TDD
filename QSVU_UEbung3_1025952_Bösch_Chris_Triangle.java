ae74e621a158b5ef73a59b624b8e525080b3ea11
Chris <chris.boesch@hotmail.com>
2013-05-23 01:56:06 +0200

    implemented method calculateHypotenuse -> run the tests -> some incorrect (small mistakes in tests, typing errors) -> after improvement all of them correct

--- QSVU_UEbung3_1025952_Bösch_Chris_Triangle.java
+++ QSVU_UEbung3_1025952_Bösch_Chris_Triangle.java
@@ -1,4 +1,5 @@
 
+
 /**
  * 
  * @author Chris Bösch
@@ -7,15 +8,16 @@
 
 public class QSVU_UEbung3_1025952_Bösch_Chris_Triangle {
 
-	public static float calculateHypotenuse(float cat1, float cat2) {
+	// hyp = sqrt(cat1^2  + cat2^2)
+	public static double calculateHypotenuse(double cat1, double cat2) {
+		return (Math.sqrt(Math.pow(cat1,2) + Math.pow(cat2,2)));
+	}
+
+	public static double calculateAngle(double angle) {
 		return 0;
 	}
 
-	public static float calculateAngle(float angle) {
-		return 0;
-	}
-
-	public static float calculateArea(float cat1, float cat2) {
+	public static double calculateArea(double cat1, double cat2) {
 		return 0;
 	}
 	
--- QSVU_UEbung3_1025952_Bösch_Chris_TriangleTest.java
+++ QSVU_UEbung3_1025952_Bösch_Chris_TriangleTest.java
@@ -9,6 +9,8 @@ import org.junit.Test;
  */
 
 public class QSVU_UEbung3_1025952_Bösch_Chris_TriangleTest {
+	
+	private double DELTA = 1e-15;
 
 	//_________ HYPOTENUSE __________
 	//to calculate:  hyp = sqrt(cat1^2  + cat2^2)
@@ -16,32 +18,31 @@ public class QSVU_UEbung3_1025952_Bösch_Chris_TriangleTest {
 	//__________ testing with cat1 > 0 AND cat2 > 0 __________
 	@Test
 	public void calcHypotenuseWithPositiveCats_returnRightLength() {
-		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(2, 2) + Math.pow(3, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(2, 3) );
-		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(1000, 2) + Math.pow(700, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(1000, 700) );
-		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(0.04, 2) + Math.pow(0.8, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(0.04f, 0.8f) );
+		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(1000, 2) + Math.pow(700, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(1000, 700), DELTA );
+		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(0.04, 2) + Math.pow(0.8, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(0.04, 0.8), DELTA );
 	}
 	
 	//__________ testing with cat1 > 0 AND cat2 > 0 __________
 	@Test
 	public void calcHypotenuseWithNegativeCats_returnRightLength() {
-		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(-8, 2) + Math.pow(-1, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(-9, 2) );
-		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(-200, 2) + Math.pow(-900, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(-200, -900) );
-		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(-0.5, 2) + Math.pow(-0.07, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(-0.5f, -0.07f) );
+		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(-8, 2) + Math.pow(-1, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(-8, -1), DELTA );
+		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(-200, 2) + Math.pow(-900, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(-200, -900), DELTA );
+		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(-0.5, 2) + Math.pow(-0.07, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(-0.5, -0.07), DELTA );
 	}
 	
 	//__________ testing with cat1 = 0 OR cat2 = 0 __________
 	@Test
 	public void calcHypotenuseWithZeroCats_returnRightLength() {
-		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(0, 2) + Math.pow(23.5, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(0, 23.5f) );
-		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(-444.1, 2) + Math.pow(0, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(-444.1f, 0) );
-		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(0, 2) + Math.pow(0, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(0, 0) );
+		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(0, 2) + Math.pow(23.5, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(0, 23.5f), DELTA );
+		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(-444.1, 2) + Math.pow(0, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(-444.1, 0), DELTA );
+		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(0, 2) + Math.pow(0, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(0, 0), DELTA );
 	}
 	
 	//__________ testing with ( cat1 < 0 AND cat2 > 0 ) AND ( cat1 > 0 AND cat2 < 0 ) __________
 	@Test
 	public void calcHypotenuseWithPosAndNegCats_returnRightLength() {
-		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(-77, 2) + Math.pow(15.5, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(-77, 15.5f) );
-		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(0.45, 2) + Math.pow(-213, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(0.45f, -213) );
+		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(-77, 2) + Math.pow(15.5, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(-77, 15.5), DELTA );
+		assertEquals("HypotenuseCalculation failed",  Math.sqrt( Math.pow(0.45, 2) + Math.pow(-213, 2) ) , QSVU_UEbung3_1025952_Bösch_Chris_Triangle.calculateHypotenuse(0.45, -213), DELTA );
 	}
 	
 	
