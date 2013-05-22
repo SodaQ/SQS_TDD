5807e25ad417525666c99cb14d08fd9ad9c2ea75
Chris <chris.boesch@hotmail.com>
2013-05-23 00:49:12 +0200

    init triangle and triangleTest class

---
+++ QSVU_UEbung3_1025952_Bösch_Chris_Triangle.java
@@ -0,0 +1,22 @@
+
+/**
+ * 
+ * @author Chris Bösch
+ *
+ */
+
+public class QSVU_UEbung3_1025952_Bösch_Chris_Triangle {
+
+	public static float calculateHypotenuse(float cat1, float cat2) {
+		return 0;
+	}
+
+	public static float calculateAngle(float angle) {
+		return 0;
+	}
+
+	public static float calculateArea(float cat1, float cat2) {
+		return 0;
+	}
+	
+}
---
+++ QSVU_UEbung3_1025952_Bösch_Chris_TriangleTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+import java.util.Stack;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * 
+ * @author Chris Bösch
+ *
+ */
+
+
+public class QSVU_UEbung3_1025952_Bösch_Chris_TriangleTest {
+
+	@Before
+	public void setup() {
+	}
+
+	@After
+	public void cleanup() {		
+	}
+
+	@Test
+	public void test1() {
+	}
+}
